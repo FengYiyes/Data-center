4ff6cdab2a14b26feda0b51eb1397d3e6605e292
lvlei <1421208597@qq.com>
2019-01-25 11:03:43 +0800

    增加全局接口地址配置

--- web/DataVisualizationSystem/vuecliTest/src/api/apiPath.js
+++ web/DataVisualizationSystem/vuecliTest/src/api/apiPath.js
@@ -1,12 +1,12 @@
 export default {
   // 真实地址
   realApi: {
-    baseURL: 'http://mall.eshangtech.com:8010',
-    url: '/MobileServicePlatform/Handler/handler_ajax.ashx'
+    baseURL: '',
+    url: ''
   },
   // 测试地址
   testApi: {
-    baseURL: 'http://192.168.10.119:8010',
-    url: '/MobileServicePlatform/Handler/handler_ajax.ashx'
+    baseURL: '',
+    url: ''
   }
 }
